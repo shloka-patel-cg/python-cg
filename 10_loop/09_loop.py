5b892c39c0168fca0e29c55e11e3f73acf1963bb
Shloka <shloka.patel.cg@gmail.com>
2026-09-12 22:54:59 +0530

    added all files

--- 10_loop/09_loop.py
+++ 10_loop/09_loop.py
@@ -42,6 +42,7 @@ for i in range(1, n + 1):
         print(i)
 n = int(input("Enter a positive integer n: "))
 
+n = int(input("Enter a number: "))
 count = 0
 
 for i in range(1, n + 1):
@@ -49,3 +50,83 @@ for i in range(1, n + 1):
         count += 1
 
 print("Count of even numbers:", count)
+
+n = int(input("Enter a number: "))
+total_sum = 0
+
+for i in range(1, n + 1):
+    total_sum += i
+
+print("Sum:", total_sum)
+
+n = int(input("Enter a number: "))
+total_sum = 0
+
+for i in range(1, n + 1):
+    total_sum += i
+
+print("Sum:", total_sum)
+
+n = int(input("Enter a number: "))
+even_sum = 0
+
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        even_sum += i
+
+print("Sum of even numbers:", even_sum)
+
+n = int(input("Enter a number: "))
+odd_sum = 0
+
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        odd_sum += i
+
+print("Sum of odd numbers:", odd_sum)
+
+text = input("Enter a string: ")
+
+for char in text:
+    print(char)
+
+text = input("Enter a string: ")
+
+text = input("Enter a string: ")
+count = 0
+
+for char in text:
+    count += 1
+
+print("Total characters:", count)
+
+text = input("Enter a string: ")
+count = 0
+
+for char in text:
+    if char == "a":
+        count += 1
+
+print("Occurrences of 'a':", count)
+
+text = input("Enter a string: ")
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+count = 0
+
+for char in text:
+    if char in uppercase_letters:
+        count += 1
+
+print("Uppercase characters count:", count)
+
+for i in range(3):  # Outer loop for 3 rows
+    for j in range(4):  # Inner loop for 4 stars per row
+        print("*", end="")
+    print()  # Move to the next line after completing a row
+
+for i in range(1, 6):  # Rows: 1 to 5
+    for j in range(1, 6):  # Columns: 1 to 5
+        print(i * j, end="\t")  # Print product followed by a tab space
+    print()  # Move to the next row
+
+
